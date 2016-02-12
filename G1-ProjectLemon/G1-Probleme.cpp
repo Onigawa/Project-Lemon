8a3c98c39777951feba55d4688fa863daf2514e4
Onigawa <eric.hantala@wanadoo.fr>
2016-02-12 17:27:52 +0100

    Update G1-Probleme.cpp

--- G1-ProjectLemon/G1-Probleme.cpp
+++ G1-ProjectLemon/G1-Probleme.cpp
@@ -1,7 +1,7 @@
 #include <vector>
 #include "G1-Probleme.h"
 
-std::vector<int> Probleme::SearchContrainteVariable(const int &Achercher) // Renvoi vecteur contenant les identifiants de toute les contraintes concernant la variable donn�e
+std::vector<int> Probleme::SearchContrainteVariable(const int &Achercher) // Renvoi vecteur contenant les identifiants de toute les contraintes concernant la variable donnée
 {
     vector<int> Resultat;
     for(unsigned int i=0; i<_Contraintes.size(); i++)
@@ -15,4 +15,62 @@ std::vector<int> Probleme::SearchContrainteVariable(const int &Achercher) // Ren
         }
     }
     return Resultat;
+}
+void Probleme::chargement()
+{
+    int identifiant,code, variable;
+    string mon_fichier = "Probleme.txt";
+    ifstream fichier(mon_fichier.c_str(), ios::in);
+    if(fichier)
+    {
+        int nbcontrainte;
+        fichier >> nbcontrainte;
+        cout << nbcontrainte << endl;
+        for(int i=0; i<nbcontrainte; i++)
+        {
+            fichier >> identifiant;
+            cout << identifiant << " ";
+            fichier >> code;
+            cout << code << " ";
+            do
+            {
+                fichier >> variable;
+
+                if(variable != -1)
+                {
+                    cout << variable << " ";
+                    //mettre la variable quelque part
+                }
+            }
+            while(variable != -1);
+            cout << endl;
+        }
+
+        int nbvariable,domaine;
+        fichier >> nbvariable;
+        cout << nbvariable << endl;
+        for(int i=0; i<nbvariable; i++)
+        {
+            fichier >> variable;
+            cout << variable << " ";
+            do
+            {
+                fichier >> domaine;
+
+                if(domaine != -1)
+                {
+                    cout << domaine << " ";
+                    //mettre la valeur du domaine quelque part
+                }
+            }while(domaine != -1);
+            cout << endl;
+
+        }
+
+        fichier.close();
+    }
+    else  // sinon
+        cerr << "Erreur à l'ouverture !" << endl;
+
+    return;
 }
