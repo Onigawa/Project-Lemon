259e8fda17bc29b658da53942a26dcc83f78f18a
Onigawa <eric.hantala@wanadoo.fr>
2016-02-12 18:43:53 +0100

    Update G1-Variable.cpp

---
+++ G1-ProjectLemon/G1-Variable.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include "G1-Variable.h"
+
+
+int Variable::getIdentifiant()
+{
+    return _Identifiant;
+}
+std::vector<int> Variable::getDomaine()
+{
+    return _Domaine;
+}
+void Variable::setIdentifiant(int Identifiant)
+{
+    _Identifiant = Identifiant;
+}
+void Variable::setDomaine(std::vector<int> Domaine)
+{
+    _Domaine = Domaine;
+}
