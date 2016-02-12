67ba33030e78672994d8408c3f0f09c247fc1d7f
Onigawa <eric.hantala@wanadoo.fr>
2016-02-12 15:24:31 +0100

    Fin de mise en place des classes

--- G1-ProjectLemon/G1-Contrainte.h
+++ G1-ProjectLemon/G1-Contrainte.h
@@ -1,6 +1,22 @@
 #ifndef G1-CONTRAINTE_H_INCLUDED
 #define G1-CONTRAINTE_H_INCLUDED
+class Contrainte
+{
+    /*---Attributs---*/
+private:
+    int _Identifiant;         // Identifiant unique de la contrainte
+    int _Code;                // Code d'identification de la contrainte
+    std::vector<int> _Variables;           // Vector de stockage des variables
 
+    /*---Méthodes---*/
+    /// Methodes optionnelles mais définies dans le cas où elles pourraient être utiles pour le projet
+
+public:
+    Contrainte(){}      // Constructeur
+    Contrainte(const Contrainte* contrainte);
+    Contrainte(int Identifiant,int Code, std::vector<int> Variables);
+    ~Contrainte(){}      // Destructeur
+};
 
 
 #endif // G1-CONTRAINTE_H_INCLUDED
--- G1-ProjectLemon/G1-Variable.h
+++ G1-ProjectLemon/G1-Variable.h
@@ -1,6 +1,20 @@
 #ifndef G1-VARIABLE_H_INCLUDED
 #define G1-VARIABLE_H_INCLUDED
 
+class Variable
+{
+    /*---Attributs---*/
+private:
+    int _Identifiant;         // Identifiant unique de la Variable
+    std::vector<int> _Domaine;           // Vector de stockage du domaine de definition
 
+    /*---Méthodes---*/
+    /// Methodes optionnelles mais définies dans le cas où elles pourraient être utiles pour le projet
+
+public:
+    Variable(){}      // Constructeur
+    Variable(const Variable* Variable);
+    Variable(int Identifiant, std::vector<int> Domaine);
+    ~Variable(){}      // Destructeur
 
 #endif // G1-VARIABLE_H_INCLUDED
